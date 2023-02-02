b639f5303f4f4ca3e20f64064d530a041aa41e7b
minyoungshin <mins6649@gmail.com>
2023-02-02 10:07:43 -0500

    scoping

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+//.toUpperCase returns a NEW STRING, so we must set it back to customerName
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "ben"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "always ben"
+}
